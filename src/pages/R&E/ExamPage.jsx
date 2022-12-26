cd1cb090cfb4368120df987693ec36337d0c06ca
smriti jojo <you@example.com>
2022-12-26 15:05:50 +0530

    exam Page done

--- src/pages/R&E/ExamPage.jsx
+++ src/pages/R&E/ExamPage.jsx
@@ -41,6 +41,15 @@ const ExamPage= () => {
   }, []);
   const Options=["A)4","B)5","C)6","D)8"];
   const[active,setactive]=useState([]);
+  const [isSelected,setSelected]=useState(false);
+
+
+  const handleClick=()=>{
+
+  }
+
+  
+  
 
   return (
     <>
@@ -49,10 +58,10 @@ const ExamPage= () => {
           className={`flex flex-col w-full min-h-screen bg-gray-200 relative transition-all overflow-hidden ease-linear duration-300`}
         >
           
-          <div className="w-full flex text-sm font-semibold bg-red-400 text-gray-600 ">
+          <div className="w-full flex text-sm font-semibold text-gray-600 justify-between">
             
-            <div><img src={Logo} width="200px" height="200px"/></div>
-            <div className="flex flex-col px-6 cursor-pointer py-8 items-end gap-[1px] ">
+            <div><img src={Logo} width="200px" height="200px" className="p-4  sm:ml-2"/></div>
+            <div className="flex flex-col px-6 cursor-pointer py-8 items-end gap-[1px] mr-6">
               <span>Time Remaining</span>
               <span>50:00</span>
             
@@ -68,30 +77,35 @@ const ExamPage= () => {
                <p className="text-slate-400 ">Question 11 of 22</p>
                <p className="text-slate-400">Reading Comprehension </p>
 
-               <div className="box-border w-50 h-50 border-4 p-8 m-8 border-yellow-300 rounded-lg shadow-red-400 shadow-2xl  sm:m-2">
+               <div className="box-border w-50 h-50 border-4 sm:p-8 px-2 py-4 m-8 border-yellow-300 rounded-lg shadow-red-400 shadow-2xl sm:m-2">
                {/* < span className="inline grid grid-cols-2 gap-4"> */}
                
                 <p>1) How many Children are swimming?</p><br></br>
 
-             
           
+                <div className="grid sm:grid-cols-2 sm:grid-rows-2 grid-cols-1 gap-4">
                 {Options.map((item,index)=>{
+                  // console.log(Options);
+                  // const data=[...Options];
+                  // console.log("Hello",data.index);
                   return(
-                    < span className="flex flex-col">
+                    // < span className="flex flex-col">
                      
-                      <Card style={active===index? {background:"yellow",marginBottom:"1rem",marginLeft:"1rem",padding:"1rem"}:{marginBottom:"1rem",marginLeft:"1rem",padding:"1rem"}} key={index} onClick={()=>setactive(index)}>{item}</Card>
-                      {/* <Card>{item}</Card>
-                      <Card>{item}</Card> */}
-                     </span>
+                      <Card className={`!transition-all !mx-4 !p-4 !duration-200 !ease-linear ${active === index ? '!bg-purple-400' : "!bg-slate-100"}`} key={index} onClick={()=>setactive(index)}>{item}</Card>
+                    //  <Card2 item={item}/>
+                  
                   )
+                  // console.log(data);
                   })}
                  
+                </div>
+                 
                </div>
-               <div>
-                <Stack spacing={5} direction="row" >
+               <div className="mt-4 flex w-full gap-3 justify-end pr-4">
+                {/* <Stack spacing={5} direction="row" > */}
                <Button variant="contained">Previous</Button>
                <Button variant="contained">Next</Button>
-               </Stack>
+               {/* </Stack> */}
 </div>
             </div>
             </div>
@@ -101,5 +115,14 @@ const ExamPage= () => {
     
   );
 };
+
+const Card2 = ({item}) => {
+  const [s, setS] = useState(false)
+  return (
+    <div onClick={() => setS(prev => !prev)} className={`px-4 py-2 ${s ? "bg-red-600" : "bg-slate-600"} text-white`}>
+      {item}
+    </div>
+  )
+}
 
 export default ExamPage;
