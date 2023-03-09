5f167a7538cee5b916c75f19028a37d955c7a459
adesh sharma <adeshs@eupheus.in>
2023-03-09 20:40:11 +0530

    changed to prod url

--- src/instance.js
+++ src/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://livesbel.xamcheck.com/app/",
+  baseURL: "https://live.classklap.com/app/",
 });
 
 export default instance;
